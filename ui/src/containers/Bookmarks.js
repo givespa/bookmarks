7d7d7c8154441cc51021715e64ef1e875faa9ce4
Jose Gil <josefcogil27@gmail.com>
2022-02-07 12:10:15 -0400

    feat: load bookmarks

--- ui/src/containers/Bookmarks.js
+++ ui/src/containers/Bookmarks.js
@@ -1,11 +1,30 @@
 import { BookmarkForm } from '../components/form';
 import { BookmarkList } from '../components/list';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // Note: API calls should be done here, 'axios' is already added as dependency
 
-export const Bookmarks = () => (
+export const Bookmarks = () => {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const getBookmarks = async () => {
+    const response = await axios.get('http://localhost:3001/bookmarks');
+    setBookmarks(response.data.bookmarks);
+  };
+
+  useEffect(() => {
+    getBookmarks();
+
+    return () => {
+      setBookmarks([]);
+    };
+  }, []);
+
+  return (
     <>
-        <BookmarkForm />
-        <BookmarkList />
+      <BookmarkForm />
+      <BookmarkList bookmarks={bookmarks} />
     </>
-);
+  );
+};
