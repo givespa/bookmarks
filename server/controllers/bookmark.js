b01576715b525069b1c8e219abb86d0313e5c9d3
Jose Gil <josefcogil27@gmail.com>
2022-02-07 11:35:19 -0400

    feat: get bookmarks

--- server/controllers/bookmark.js
+++ server/controllers/bookmark.js
@@ -1,11 +1,20 @@
+// requires modules to connect to the store
+const fs = require('fs/promises');
+const path = require('path');
+
+// store path
+const filePath = path.join(__dirname, '../store/users.json');
+
 const getBookmarks = (req, res) => {
-    // recover all bookmarks here
-    res.send([]);
+  // recover all bookmarks here
+  const bookmarks = require('../store/bookmarks.json');
+
+  res.json({ ok: true, bookmarks });
 };
 
 const addBookmark = (req, res) => {
-    // add the new bookmark, give it an ID and return it
-    res.send({});
+  // add the new bookmark, give it an ID and return it
+  res.send({});
 };
 
 module.exports = { getBookmarks, addBookmark };
