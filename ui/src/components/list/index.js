34115ee97498ca260f36baa7f540218948858321
Jose Gil <josefcogil27@gmail.com>
2022-02-07 12:33:27 -0400

    feat: bookmarks list

--- ui/src/components/list/index.js
+++ ui/src/components/list/index.js
@@ -1,7 +1,25 @@
-export const BookmarkList = () => {
-    return (
-        <>
-            {/* Your code goes here */}
-        </>
-    );
-};
+export const BookmarkList = ({ bookmarks }) => (
+  <>
+    <div className="row">
+      {bookmarks.length > 0 &&
+        bookmarks.map((bookmark, index) => (
+          <div className="col-md-3" key={index} href={bookmark.link}>
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text">
+                  <a
+                    href={bookmark.link}
+                    style={{ textDecoration: 'none' }}
+                    className="text-black"
+                    target="_blank"
+                  >
+                    {bookmark.title}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  </>
+);
