9a3ec74a9fa0fd7580278322a660fe890d6375c9
Jose Gil <josefcogil27@gmail.com>
2022-02-07 12:06:20 -0400

    feat: enable cors

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bookmarkRoute = require('./routes/bookmark');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/bookmarks', bookmarkRoute);
 
 /* istanbul ignore next */
 app.get('/', (req, res) => {
-	res.send('OK!');
+  res.send('OK!');
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(3001, () => {
-  console.log(`Server has been started on 3000!`);
+  console.log(`Server has been started on 3001!`);
 });
