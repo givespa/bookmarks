93a4881ca698ec863eecd725e6b86ee8ac563264
Jose Gil <josefcogil27@gmail.com>
2022-02-07 11:52:09 -0400

    feat: add a new bookmark

--- server/controllers/bookmark.js
+++ server/controllers/bookmark.js
@@ -1,9 +1,11 @@
+const uuid = require('uuid').v4;
+
 // requires modules to connect to the store
 const fs = require('fs/promises');
 const path = require('path');
 
 // store path
-const filePath = path.join(__dirname, '../store/users.json');
+const filePath = path.join(__dirname, '../store/bookmarks.json');
 
 const getBookmarks = (req, res) => {
   // recover all bookmarks here
@@ -12,9 +14,31 @@ const getBookmarks = (req, res) => {
   res.json({ ok: true, bookmarks });
 };
 
-const addBookmark = (req, res) => {
+const addBookmark = async (req, res) => {
   // add the new bookmark, give it an ID and return it
-  res.send({});
+  const { title, link } = req.body;
+
+  let bookmarks = require('../store/bookmarks.json');
+
+  const newBookmark = {
+    id: uuid(),
+    title,
+    link,
+  };
+
+  bookmarks.push(newBookmark);
+
+  try {
+    await fs.writeFile(filePath, JSON.stringify(bookmarks));
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      ok: false,
+      message: 'Error',
+    });
+  }
+
+  return res.json({ ok: true, newBookmark });
 };
 
 module.exports = { getBookmarks, addBookmark };
